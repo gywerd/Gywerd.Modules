aa8080d132982fc84fc05ff28c6c45f302153df0
Baron Daniel Bernhard Rugbjerg Gammelgaard Giversen <bern0145@edu.campusvejle.dk>
2017-11-22 19:36:12 +0100

    Finished app...

--- Gywerd.Modules.DataAccess/ConfigurationManager.cs
+++ Gywerd.Modules.DataAccess/ConfigurationManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,8 +19,24 @@ namespace Gywerd.Modules.DataAccess
         /// <returns>string</returns>
         public static string GetConnectionString(string path, string name = null)
         {
-            string res = "";
-            return res;
+            if (name.Equals(null))
+            {
+                return "Data Source=10.205.44.39,49172;Initial Catalog=DanielWeb2DbApp;Integrated Security=False;User ID=Aspit;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            }
+            else
+            {
+                // Assume failure.
+                string returnValue = null;
+
+                // Look for the name in the connectionStrings section.
+                ConnectionStringSettings settings =
+                    ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+                // If found, return the connection string.
+                if (settings != null)
+                    returnValue = settings.ConnectionString;
+                return returnValue;
+            }
         }
         #endregion
     }
